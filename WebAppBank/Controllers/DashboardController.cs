e6aa679de7d2c65ccee4ad569cf3e924787334b6
itishree2 <nayakitishree6262@gmail.com>
2024-02-24 10:27:17 +0530

    Add project files.

---
+++ WebAppBank/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppBank.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout() 
+        {
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ WebAppBank/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppBank.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            return RedirectToAction("Dashboard","Home",new { username= username});
+        }
+    }
+}
